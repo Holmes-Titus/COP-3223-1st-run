176c57ec59413e0735983f09c7c3bbe89ead28d8
Titus Holmes <ti431462@ucf.edu>
2026-05-27 19:07:40 +0000

    modified:   tester.c

--- tester.c
+++ tester.c
@@ -3,6 +3,8 @@
 
 int test_function(int hit1, int hit2); // prototype or decleration (names of variables are optional but preferred)
 
+int score = 0; //score is a glbal variable that can be modified anywhere
+
 int main(void){
 
     
@@ -12,5 +14,12 @@ int main(void){
 
 int test_function(int hit1, int hit2){ //definition (the actual work)
     //arguments are copied into the function, doesn't manipulate the actual value
+
+    // arrays are pointers, so they son't manipulate a copy but the real thing
+
+    //scope is a region of code where a name is visible and usable. Ex: hit1 has a scope of the test_function
+
+    //
+
     return 0;
 }
