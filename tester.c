ce136b68148ba1ef5c3d909568db46d8be2aabf8
Titus Holmes <ti431462@ucf.edu>
2026-05-27 18:43:57 +0000

    modified:   tester.c

--- tester.c
+++ tester.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int test_function(int hit1, int hit2); // prototype or decleration (names of variables are optional but preferred)
+
 int main(void){
 
     
 
     return;
+}
+
+int test_function(int hit1, int hit2){ //definition (the actual work)
+    //arguments are copied into the function, doesn't manipulate the actual value
+    return 0;
 }
