20f18fe16b7da0dca2fa0adfac5a354cad91b8c9
Titus Holmes <ti431462@ucf.edu>
2026-06-10 19:15:46 +0000

    new file:   ex2 	modified:   ex2_output_params.c 	new file:   w5b 	new file:   w5b.c

--- ex2_output_params.c
+++ ex2_output_params.c
@@ -8,15 +8,13 @@ int temp = *a;
 *b = temp;
 }
 void stats(const int arr[], int len, int *out_min, int *out_max,double *out_avg){ //only will work for all values >0
-    int temp1 = *out_min;
-    temp1 = 0;
-    int temp2 = *out_max;
-    temp2 = 0;
-    double avg = *out_avg;
-    avg = 0;
-    for (int i=0;i<len;i++){
-        if (arr[i] < temp1)*out_min = arr[i];
-        if (arr[i] > temp2)*out_max = arr[i];
+    *out_min = arr[0];
+    *out_max = arr[0];
+    for (int i=1;i<len;i++){
+        if (arr[i] < *out_min){
+            *out_min = arr[i];
+        }
+        if (arr[i] > *out_max)*out_max = arr[i];
         *out_avg += arr[i];
     }
     *out_avg /= (double) len;
@@ -29,11 +27,11 @@ int main(void){
     printf("swap: x=%d and y= %d\n",x,y);
 
     int data[] = {34, 12, 78, 56, 23};
-    int len = sizeof(data);
-    int out_min;
-    int out_max;
-    double out_avg;
-    stats(data, len,&out_min,&out_max,&out_avg);
-    printf("min=%d, max=%d, avg=%.2f\n",out_min,out_max,out_avg);
+    int len = sizeof(data)/sizeof(data[0]);
+    double avg;
+    int min;
+    int max;
+    stats(data, len,&min,&max,&avg);
+    printf("min=%d, max=%d, avg=%.2f\n",min,max,avg);
 
 }
---
+++ w5b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+    typedef struct {char* name; int hp; int max_hp;} Enemy;
+    Enemy goblin = {.name = "Goblin", .hp = 40, .max_hp = 40};
+    Enemy *p    = &goblin;
+    (*p).hp = 30;    //will change goblin.hp to 30, dereferencing p gives back the whole struct
+    // *p.hp this references incorrectly as it works in the wrong order, equivelent to *(p.hp)
+    p->hp == (*p).hp;// true
+    p->name == (*p).name;// true
+    p->hp -= 10;
+    printf("%s HP: %d\n",p->name,p->hp);
+    //  -> is almost always used for pointer to struct access
+    //  . is on a struct variable,  -> is on a pointer to a struct
+    
+    //pass-by-value will copy the struct
+    //if you pass a pointer instead it avoids the copy, if you don't want ti to edit make it const/read-only
+    // passing a pointer only copies the 8 bytes of the pointer
+    int x;
+    int y;
+    x = y = 5;
+    printf("%d is the sum of x and y\n",x+y);
+}
