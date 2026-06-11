30f226676fee5087b592cbd073e80246f7a71f2a
Titus Holmes <ti431462@ucf.edu>
2026-06-11 23:18:09 +0000

    modified:   mathlib.c 	modified:   testme 	modified:   testme.c 	modified:   toolkit

--- mathlib.c
+++ mathlib.c
@@ -14,8 +14,15 @@ double power(double base, int exp) {
 }
 
 int is_prime(int n) {
-    /* TODO */
-    return 0;
+    double check;
+    if (n<2) return 0;
+    for (int i =2; i<(n/2)+1;i++){
+        check = ((double)n)/i;
+        if (check == (int) check){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int gcd(int a, int b) {
@@ -29,6 +36,12 @@ double average(int arr[], int len) {
 }
 
 int count_digits(long long n) {
-    /* TODO */
-    return 0;
+    if (n==0) return 1;
+    int count = 0;
+    if (n<0) n*= (-1);
+    while (n > 0){
+        n/=10;
+        count++;
+    }
+    return count;
 }
--- testme.c
+++ testme.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(void){
-    int x; printf("Enter count then values: ");  fscanf(stdin, "%d", &x); int values[x];    for (int i =0;i<x;i++) fscanf(stdin, "%d", &values[i]); 
-    printf("%d\n",values[3]);
+    //int x; printf("Enter count then values: ");  fscanf(stdin, "%d", &x); int values[x];    for (int i =0;i<x;i++) fscanf(stdin, "%d", &values[i]); 
+   // printf("%d\n",values[3]);
+   // double z = 8.0;
+   // printf("%d",(z==(int)z));
 }
