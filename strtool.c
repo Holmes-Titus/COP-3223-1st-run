29be1ba690b7d5b9a0d37fdc50584e765d9a18d5
Titus Holmes <ti431462@ucf.edu>
2026-06-12 18:11:53 +0000

    modified:   strtool 	modified:   strtool.c

--- strtool.c
+++ strtool.c
@@ -24,7 +24,7 @@ int main(void) {
         
         
         switch (choice) {
-            case 1: {char length[63];  getchar();  printf("Enter string: "); fgets(length,sizeof(length),stdin);    printf("Length: %d\n",str_length(length)); break;}
+            case 1: {char length[63];  printf("Enter string: "); fgets(length,sizeof(length),stdin);    printf("Length: %d\n",str_length(length)); break;}
             case 2: {char *dst = "";char *src = ""; int dst_size = 0;  printf("Enter dst, then src, then dst size: ");   fscanf(stdin, "%63s,%63s,%d", dst,src,&dst_size);    str_copy(dst,src,dst_size); printf("string copied.\n"); break;}
             case 3: {char *a = "";char *b ="";  printf("Enter string: ");   fscanf(stdin, "%63s,%63s", a,b);    int result = str_compare(a,b); if (result){printf("%s comes after %s\n",a,b);} if (result < 0) {printf("%s comes before %s\n",a,b);} else{printf("%s equals %s\n",a,b);} break;}
             case 4: {char *s = ""; printf("Enter a and b: ");   fscanf(stdin, "%63s", s);  str_reverse(s);  printf("Reversed: %s\n",s); break;}
@@ -48,6 +48,7 @@ int get_validated_int(int min, int max, const char *prompt, const char *err){
     do {
     printf("%s", prompt);   //prompts an input
     fscanf(stdin, "%d", &pick); //takes in the input to integer pick
+    getchar();
     if (pick < min || pick > max){  //checks if pick is not within given range
         printf("%s\n", err);    //returns error message if not within range
     }
